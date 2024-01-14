fcb4d06dccb8e6799161e82b089798f82b418f06
Ender-Vels <endervels1999@gmail.com>
2024-01-14 20:17:39 +0200

    dev

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -26,17 +26,68 @@
             </div>
         </div>
        </div>
+       <div class="mainPage_presents_day">
+            <h1>Пропозиція тижня:</h1>
+       </div>
+       <div class="mainPage_presents_day_container">
+       <div v-for="(phone,index) in arrayPhone" :key="index">
+
+        <ProductCard
+         :title="phone.title"
+         :photoURL="phone.photoURL"
+         :price="phone.price"
+          />
+
+       </div>
     </div>
+    </div>
+
     <RouterView/>
 </template>
 
 <script>
+import ProductCard from './ProductCard.vue'
 
-
-
-
-
-
+export default{
+    components:{
+        ProductCard,
+    },
+    data(){
+        return{
+            arrayPhone:[
+                {
+                    title:'Apple iPhone 14 Pro 128 ГБ Б/У (Deep Purple) Ідеальний стан',
+                    photoURL:"../assets/product/14PM.png",
+                    price: '25 990',
+                },
+                {
+                    title:'Apple iPhone 14 Pro 128 ГБ Б/У (Deep Purple) Ідеальний стан',
+                    photoURL:"../assets/product/14PM.png",
+                    price: '25 990',
+                },
+                {
+                    title:'Apple iPhone 14 Pro 128 ГБ Б/У (Deep Purple) Ідеальний стан',
+                    photoURL:"../assets/product/14PM.png",
+                    price: '25 990',
+                },
+                {
+                    title:'Apple iPhone 14 Pro 128 ГБ Б/У (Deep Purple) Ідеальний стан',
+                    photoURL:"../assets/product/14PM.png",
+                    price: '25 990',
+                },
+                {
+                    title:'Apple iPhone 14 Pro 128 ГБ Б/У (Deep Purple) Ідеальний стан',
+                    photoURL:"../assets/product/14PM.png",
+                    price: '25 990',
+                },{
+                    title:'Apple iPhone 14 Pro 128 ГБ Б/У (Deep Purple) Ідеальний стан',
+                    photoURL:"../assets/product/14PM.png",
+                    price: '25 990',
+                },
+            ]
+        }
+    }
+}
 
 </script>
 
@@ -59,11 +110,15 @@
     padding: 15px;
     border-radius: 10px;
 }
+.mainPage_conteiner_present_link h1{
+    margin-left:20px ;
+}
 .mainPage_conteiner_present_main_product{
     display: flex;
-    justify-content: space-between;
+    justify-content:space-between;
     margin: 15px;
 }
+
 .mainPage_conteiner_present_main_product img{
     height: 200px;
 }
@@ -94,4 +149,19 @@
 
 }
 
+.mainPage_presents_day_container{
+    display: grid;
+   grid-template-columns: 1fr 1fr 1fr;
+}
+.mainPage_presents_day{
+    margin: 40px;
+    padding: 20px;
+}
+.mainPage{
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    
+
+}
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="productCard">
+        <div class="productCard_box">
+            <div class="productCard_box_product">
+                <div>
+                <img :src="urlP" alt="">
+            </div>
+                <div class="productCard_box_product_title">
+                <b>{{ title }}</b>
+            </div>
+            <div class="productCard_box_product_price">
+               <b> {{ price }} <span>грн</span></b>
+            </div>
+            <div class="productCard_Buy">
+                <button>
+                   <b> + Купити</b>
+                </button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props:['title','photoURL','price'],
+    data(){
+        return{
+            urlP:"https://www.istore.ua/upload/iblock/86d/z81r0rkob7d3x3dtqer9p03airnm7hub/14_pro_max_gpurpl_3_is.png"
+        }
+    }
+        }
+
+</script>
+
+<style>
+.productCard{
+    display: grid;
+    
+}
+
+.productCard_box_product{
+    margin: 40px;
+    font-size: 20px;
+    max-width: 450px;
+    align-items: center;
+    text-align: center;
+    background-color:#f3f3f3;
+    border-radius: 10px;
+}
+.productCard_box_product img{
+    height: 300px;
+    width: 300px;
+}
+.productCard_box_product_title{
+    color:#051726;
+    font-size: 30px;
+    margin: 15px;
+    
+}
+.productCard_box_product_price{
+    color:#051726;
+    font-size: 30px;
+    margin: 15px;
+}
+.productCard_box_product:hover{
+    -webkit-box-shadow: 10px 10px 63px -3px rgba(0,245,114,1);
+-moz-box-shadow: 10px 10px 63px -3px rgba(0,245,114,1);
+box-shadow: 10px 10px 63px -3px rgba(0,245,114,1);
+background-color:rgb(157, 230, 191);
+}
+.productCard_Buy button{
+    background-color: #01ff70;
+    color: antiquewhite;
+    font-size: 25px;
+    border: none;
+    border-radius:5px;
+    padding:15px;
+    margin: 25px;
+    cursor: pointer;
+}
+
+</style>
